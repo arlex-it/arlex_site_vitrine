3f71e3a8a7c0df32d28e9f0df065bc40939de0ff
Charles Castelain <faxxiz@localhost.localdomain>
2019-06-09 11:37:14 +0200

    jquery added to scroll auto on divs + jpgs

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,19 @@
-$('#link-notre-equipe').on('click', function () {
-    alert("works");
-    $('body').animate({
-        scrollTop: $(".notre-equipe").offset().top
-    }, 'slow');
-});
+$(function () {
+    $('#link-notre-equipe').on('click', function () {
+        $('html,body').animate({
+            scrollTop: $(".notre-equipe").offset().top - 150
+        }, 'slow');
+    });
 
-$('#link-contact').on('click', function () {
-    alert("works");
-    $('body').animate({
-        scrollTop: $(".contact").offset().top
-    }, 'slow');
-});
+    $('#link-contact').on('click', function () {
+        $('html,body').animate({
+            scrollTop: $(".contact").offset().top - 150
+        }, 'slow');
+    });
 
-$('#link-faq').on('click', function () {
-    alert("works");
-    $('body').animate({
-        scrollTop: $(".faq").offset().top
-    }, 'slow');
+    $('#link-faq').on('click', function () {
+        $('html,body').animate({
+            scrollTop: $(".faq").offset().top - 150
+        }, 'slow');
+    });
 });
-
-function linkcontact() {
-    alert("zzz");
-    $('html,body').animate({
-        scrollTop: $(".notre-equipe").offset().top
-    }, 'slow');
-}
